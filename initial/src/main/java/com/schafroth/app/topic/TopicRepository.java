de5d7741e28a6f4056eff94baa66e9fb367be219
Dennis Schafroth <dennis@schafroth.dk>
2018-09-04 17:31:30 +0200

    Add Topic Repository (DB table mapping)

---
+++ initial/src/main/java/com/schafroth/app/topic/TopicRepository.java
@@ -0,0 +1,11 @@
+package com.schafroth.app.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+//This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+//CRUD refers Create, Read, Update, Delete
+
+public interface TopicRepository extends CrudRepository<Topic, Integer> {
+
+
+}
