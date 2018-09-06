f8ef48aa957df99fd1009805a4038776a3ecd02d
Dennis Schafroth <dennis@schafroth.dk>
2018-09-06 13:25:17 +0200

    Use Logger

--- initial/src/main/java/com/schafroth/app/topic/messaging/jms/ReplyEmailSenderJMS.java
+++ initial/src/main/java/com/schafroth/app/topic/messaging/jms/ReplyEmailSenderJMS.java
@@ -1,27 +1,33 @@
 package com.schafroth.app.topic.messaging.jms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.schafroth.app.topic.ReplyEmail;
 import com.schafroth.app.topic.ReplyEmailSender;
 
 @RestController
-@RequestMapping("/transaction")
-@Profile("Step6") 
-public class ReplyEmailSenderJMS implements ReplyEmailSender
+// @RequestMapping("/transaction")
+@Profile("Step6")
+public class ReplyEmailSenderJMS implements ReplyEmailSender 
 {
-  @Autowired private JmsTemplate jmsTemplate;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReplyEmailSenderJMS.class);
 
-  @PostMapping("/send")
-  public void send(@RequestBody ReplyEmail replyEmail) {
-    System.out.println("Queuing a reply email.");
-    // Post message to the message queue named "OrderTransactionQueue"
-    jmsTemplate.convertAndSend("ReplyEmailQueue", replyEmail);
-  }
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@PostMapping("/send")
+	public void send(@RequestBody ReplyEmail replyEmail) {
+		String queueName = "ReplyEmailQueue";
+		LOGGER.info("sending payload='{}' to JMS queue='{}'", replyEmail, queueName);
+		// Post message to the message queue named "OrderTransactionQueue"
+		jmsTemplate.convertAndSend(queueName, replyEmail);
+	}
 }
