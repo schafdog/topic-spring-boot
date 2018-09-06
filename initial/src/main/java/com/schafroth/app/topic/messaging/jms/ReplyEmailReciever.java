f7f0db4dad054722e02f4eaf1653e07fddbe46a6
Dennis Schafroth <dennis@schafroth.dk>
2018-09-06 13:23:57 +0200

    Fix spelling

--- initial/src/main/java/com/schafroth/app/topic/messaging/jms/ReplyEmailReciever.java
+++ initial/src/main/java/com/schafroth/app/topic/messaging/jms/ReplyEmailReciever.java
@@ -1,20 +1,24 @@
 package com.schafroth.app.topic.messaging.jms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import com.schafroth.app.topic.ReplyEmail;
 import com.schafroth.app.topic.messaging.MailClient;
-
 @Component
-public class ReplyEmailReceiver {
+public class ReplyEmailReciever {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReplyEmailReciever.class);
+
 	@Autowired
 	private MailClient smtpClient;
 
 	@JmsListener(destination = "ReplyEmailQueue")
 	public void receiveMessage(ReplyEmail replyEmail) {
-		System.out.println("Received <" + replyEmail + ">");
+		LOGGER.info("Received " + replyEmail + "for SMTP delivering");
 		smtpClient.deliver(replyEmail);
 	}
 }
